34963c3be540358b25b02e946589490a4e6cf7c8
Equinoxs <76661986+Equinoxs@users.noreply.github.com>
2021-03-24 13:56:39 +0100

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,16 @@
+REM Remplacer PROGRAMME par le nom du programme à installer (regarder le nom dans WinGet)
+REM Recopier : winget install PROGRAMME
+REM            if %ERRORLEVEL% EQU 0 Echo PROGRAMME a été installé correctement. 
+REM autant de fois que de programmes
+
+
+@echo off  
+Echo Installer les applications
+
+  
+winget install PROGRAMME
+if %ERRORLEVEL% EQU 0 Echo PROGRAMME a été installé correctement. 
+
+
+
+Echo ---Script par Equinoxs, en 2021---
